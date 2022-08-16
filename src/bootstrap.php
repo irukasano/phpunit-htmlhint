b2cb3664788e0aa3987c9815b321980bf4b32ba6
sano <sano@iruka-system.co.jp>
2022-08-16 11:45:07 +0900

    update bootstrap.php
    * Dotenv loader by createUnsafeMutable

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 try {
-    $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__.DIRECTORY_SEPARATOR."..");
+    $dotenv = \Dotenv\Dotenv::createUnsafeMutable(__DIR__."/../");
     $dotenv->load();
 } catch (\Dotenv\Exception\InvalidPathException $e) {
 }
